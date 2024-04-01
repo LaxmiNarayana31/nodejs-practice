7e76d74e818610b233e86dff784e8a427716da72
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-04-01 21:43:05 +0530

    make a basic server in node js

--- Basics/03_index.js
+++ Basics/03_index.js
@@ -1,8 +1,8 @@
 // About Package.json file
 
 /* package.json file store your project details. Like project name, version
-what are commands used, what are package inside */
+what are commands used, what are package inside 
 
-// use "npm init" command to create package json file
+use "npm init" command to create package json file
 
-// what is node_modules folder deleted. -> run npm install command in terminal all modules are created
+Note: what is node_modules folder deleted. -> run npm install command in terminal all modules are created */
