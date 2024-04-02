8d924d96d23ac5c91c7ad8d889d1cd0a22cfffd9
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-04-02 19:44:25 +0530

    Nodemon package

---
+++ 01_Basics/04_index.js
@@ -0,0 +1,13 @@
+/* Nodemon package
+
+When you build a node js project when run the project it show output.
+When again you do some changes in project you again run the project. This was time consuming -> To save this we use nodemon 
+
+nodemon continuesly run your node js project 
+
+To use this:
+Install nodemon: npm i nodemon (OR) npm i nodemon -g (it means it is installed in globally. When you use this it is installed locally in whole system or no need to install in separate folder)
+
+After installing if you want to run any file ->use command:  nodemon <file_name>
+
+*/
