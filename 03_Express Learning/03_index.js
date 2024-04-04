5499757205f3a7efcf0a2145ff0be47193c17e94
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-04-04 08:17:09 +0530

    Render HTML and JSON

---
+++ 03_Express Learning/03_index.js
@@ -0,0 +1,35 @@
+// Render HTML and JSON
+
+const express = require('express');
+const app = express();
+
+app.get("", (req, resp) => {
+    resp.send(`
+    <h1>Welcome to Home page</h1>
+    <a href="/about">Go to About page</a>
+    `);
+});
+
+app.get("/about", (req, resp) => {
+    resp.send(`
+    <input type="text" placeholder="user name" value="${req.query.name}"  />
+    <button>Click</button>
+    <a href="/">Go to Home page</a>
+    `);
+});
+
+app.get("/help", (req, resp) => {
+    resp.send([
+        {
+            name:'shiva',
+            email:'shiva@gmail.com'
+        },
+        {
+            name:'sumit',
+            email:'sumit@gmail.com'
+        }
+    ]);
+});
+
+
+app.listen(5000);
