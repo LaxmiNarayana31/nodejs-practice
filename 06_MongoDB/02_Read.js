5d4fcf64df94504900292e7d34801560217321d2
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-04-10 17:02:05 +0530

    Read data from MongoDB to node js

---
+++ 06_MongoDB/02_Read.js
@@ -0,0 +1,21 @@
+// Read data from MongoDB to node js
+
+const dbConnect = require("./mongodb");
+
+// // 1st way to handle promises
+// dbConnect().then((resp) => {
+//     resp.find().toArray().then((data) => {
+//         console.log(data)
+//     })
+// })
+// console.log(dbConnect());
+
+
+// 2nd way to handle promises (Modern Syntax)
+const main = async () => {
+  let data = await dbConnect();
+  data = await data.find({ name: "nord" }).toArray();
+  console.log(data);
+};
+
+main();
