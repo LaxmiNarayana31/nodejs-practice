650512d63277d647275f2bf0c8bc8782184008de
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-04-11 20:27:58 +0530

    get method in API

---
+++ 07_API with MongoDB/api.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const dbConnect = require("..06_MongoDB/mongodb");
+
+const app = express();
+
+
+// IQ: in the get method, we can pass body or not -> No
+// http://localhost:5000/
+app.get("/", async (req, resp) => {
+  let data = await dbConnect();
+  data = await data.find().toArray();
+  console.log(data);
+  resp.send(data);
+});
+
+
+app.post("/", async (req, resp) => {
+    resp.send({name:"sumit"})
+});
+
+
+app.listen(5000);
