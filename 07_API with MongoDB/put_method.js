04ca3173030d891a1b5da3ac2659becd03211196
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-04-15 12:36:04 +0530

    update the data using put API method

---
+++ 07_API with MongoDB/put_method.js
@@ -0,0 +1,23 @@
+// put API method: Whenever you need to update the data put API method used
+
+const express = require("express");
+const dbConnect = require("..06_MongoDB/mongodb");
+
+const app = express();
+
+// how to get data in Node JS send by postman
+app.use(express.json())
+
+
+app.put('/:name', async (req, resp) => { 
+    // console.log(req.body)
+    let data = await dbConnect()
+    let result = data.updateOne((
+        {name:req.params.name},
+        {$set:req.body}
+    ))
+    resp.send({result:"update"})
+})
+
+
+app.listen(5000);
