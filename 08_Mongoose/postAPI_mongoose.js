7e1583f18b7158c378887cb9d173ead26c6cce45
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-04-16 20:20:07 +0530

    create postAPI using mongoose

---
+++ 08_Mongoose/postAPI_mongoose.js
@@ -0,0 +1,19 @@
+// create postAPI using mongoose
+
+const express = require("express");
+require('./config');
+const Produtcs = require("./products");
+
+const app = express();
+
+// Whenever data is come it comes in string format. so need to convert to JSON format
+app.use(express.json)
+
+app.post("/create", async (req, resp) => {
+    let data = Produtcs(req, resp);
+    let result = await data.save()
+    console.log(result);
+    resp.send("Done");
+})
+
+app.listen(5000)
