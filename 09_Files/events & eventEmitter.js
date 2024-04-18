571af88623995946b5703e5f72a4fc3a8d56a0cf
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-04-18 10:44:41 +0530

    event emitters

--- 09_Files/events & eventEmitter.js
+++ 09_Files/events & eventEmitter.js
@@ -1,1 +1,27 @@
-// 
+const express = require("express");
+const EventEmitter = require('events')
+const app = express();
+const event = new EventEmitter()
+
+let count = 0
+
+event.on("countAPI", () => {
+    count++
+    console.log("event called", count);
+})
+
+
+app.get("/", (req, resp) => {
+    resp.send("API Called")
+    event.emit("countAPI")
+})
+
+app.get("/search", (req, resp) => {
+    resp.send("Search API Called")
+})
+
+app.get("/update", (req, resp) => {
+    resp.send("Update API Called")
+})
+
+app.listen(5000)
