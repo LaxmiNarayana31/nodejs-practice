1b2cacab42e31bc6f4ac2169485b4a9e022ed62a
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-04-20 19:24:46 +0530

    module and its types

---
+++ 11_Notes/02_module.js
@@ -0,0 +1,14 @@
+/*
+3 types of module : 
+                    Built-in module
+                    third party module
+                    file based module
+*/
+
+const langName = "JavaScript"
+module.exports = langName
+
+
+
+// to import it another file
+const name = require("./02_module")
