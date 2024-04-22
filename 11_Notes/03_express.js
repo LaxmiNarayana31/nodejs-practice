8a909d02c1f8184cf92dad85510e2641266525a1
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-04-22 21:15:36 +0530

    Routing and status code

--- 11_Notes/03_express.js
+++ 11_Notes/03_express.js
@@ -6,7 +6,10 @@ const app = express();
 // Routing
 app.get("/", (req, res) => {
     res.send("HII");
-    res.statusCode(404)
+    // res.statusCode(404) // 404 means not found 
+    res.sendStatus(404)
+    res.sendStatus(500) // 500 means internal server error 
+
 })
 
 app.listen(5000, () => {
