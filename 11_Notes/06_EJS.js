e46bf8fdcf7cfd487cf9a0f5e475430147c1da64
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-04-22 21:18:27 +0530

    about Embedded JavaScript templates

---
+++ 11_Notes/06_EJS.js
@@ -0,0 +1,22 @@
+// EJS -> Embedded JavaScript templates
+
+const express = require("express");
+const path = require("path");
+const app = express();
+
+express.static(path.join(path.resolve(), "public")) // it is a middleware so we can't use it directly
+
+app.use(express.static(path.join(path.resolve(), "public")))
+
+// Setting up View Engine
+app.set("view engine", "ejs");
+
+// what is our complete app written in Node JS. No front-end libraries are used  
+app.get("/", (req, res) => {
+    res.render("index", {name: "Shiva"})
+})
+
+
+app.listen(5000, () => {
+    console.log("Server is working");
+})
