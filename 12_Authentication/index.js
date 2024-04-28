cadf9a3169e7781ee08ae8fdbdfc71a287c98141
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-04-28 16:57:25 +0530

    Not complete

---
+++ 12_Authentication/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+express.static(path.join(path.resolve(), "public")) // it is a middleware so we can't use it directly
+
+app.use(express.static(path.join(path.resolve(), "public")))
+
+app.get("/", (req, res) => { 
+    res.render("login")
+})
+
+app.post("/login", (req, res) => {
+    res.cookie('token', "iamin")
+    res.redirect("/")
+})
+
+app.listen(5000, () => {
+    console.log("Server is working");
+})
