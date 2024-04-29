7d296bff4649b11a216e1bdff8517bace66aaa9c
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-04-29 17:16:33 +0530

    controllers - all functions wrapped

---
+++ 13_API/controllers/user.js
@@ -0,0 +1,44 @@
+const User = require("../models/user.js")
+export const getAllUsers = async (req, resp) => { 
+    const users = await User.find({}) 
+
+    const keyword = req.query.keyword
+    console.log(keyword);
+
+    read.json({
+        success: true,
+        users,
+    })
+}
+
+export const newUser = async (req, res) => { 
+    const { name, email, password } = req.body
+    
+    await User.create({
+        name,
+        email,
+        password
+    })
+    res.status(201).cookie("tempi", "lol").json({
+        success: true,
+        message:"User created successfully"
+    })
+}
+
+
+export const specialFunc = async (req, res) => { 
+    res.json({
+       success: true,
+       message: "Hello JavScript"
+   })
+}
+
+export const getUserDetail= async (req, res) => { 
+    const { id } = req.params
+    const user = await User.findById(id)
+
+    res.json({
+        success: true,
+        user,
+    })
+}
